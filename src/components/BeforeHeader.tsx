f685eb8c42d53e564f52728d4a2ec9bfb856c9ad
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-12 15:27:02 -0300

    feat: Before Header component

---
+++ src/components/BeforeHeader.tsx
@@ -0,0 +1,10 @@
+export default function BeforeHeader() {
+  return (
+    <div className="flex h-10  bg-rose-600">
+      <div className="m-auto flex w-[84%] justify-end text-white">
+        <p>Compre Pelo telefone</p>
+        <p className="ml-2 font-semibold">(91)3085-3190</p>
+      </div>
+    </div>
+  );
+}
