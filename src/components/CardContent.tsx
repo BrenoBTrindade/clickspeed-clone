0bd3aecdc979f8c5cc8802b9f51c11b66de92665
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-13 03:02:46 -0300

    feat: cardContent component

---
+++ src/components/CardContent.tsx
@@ -0,0 +1,11 @@
+export default function CardContent() {
+  return (
+    <div className="mt-4 flex flex-col text-center text-lg text-white">
+      <p className="mb-3">BENEFÍCIOS:</p>
+      <p className="mb-3">Instalação Gratuita</p>
+      <hr className="mb-3" />
+      <p className="mb-3">Wi-fi Incluso</p>
+      <hr className="mb-3" />
+    </div>
+  );
+}
