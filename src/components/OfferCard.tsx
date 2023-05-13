6d45f48544aa0a2d3f1967aa9d52943fd2ebb5c0
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-13 03:04:03 -0300

    feat: offercard component

---
+++ src/components/OfferCard.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function OfferCard({ children }: Props) {
+  return (
+    <div className=" mb-10 ml-8 h-[594px] w-[328px] rounded-lg bg-rose-600 px-10 py-14 shadow-md shadow-gray-500 ">
+      <div className=" relative -top-16 ml-5 w-52 animate-bounce rounded-md bg-purple-950 p-2 text-center font-semibold text-white">
+        MELHOR OFERTA
+      </div>
+      <div className="-mt-10">{children}</div>
+    </div>
+  );
+}
