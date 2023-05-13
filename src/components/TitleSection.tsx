8342ada4bf8cb90c9c73d15518449bdecafea12b
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-13 03:04:17 -0300

    feat: title section component

---
+++ src/components/TitleSection.tsx
@@ -0,0 +1,7 @@
+export default function TitleSecton() {
+  return (
+    <div className="m-auto mb-10 mt-8 flex w-[85%] items-center justify-center rounded-md bg-purple-950 p-2 text-4xl text-white shadow-md shadow-gray-500">
+      <h1>NOSSOS PLANOS</h1>
+    </div>
+  );
+}
