acc4acd0041639236391fb596af6992f3b51cdf4
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-13 03:03:33 -0300

    feat: buttons components

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button className=" mt-4 w-full rounded-lg bg-gradient-to-r from-rose-500 to-purple-950 p-2 font-semibold text-white shadow-md shadow-gray-700 hover:from-pink-500 hover:to-yellow-500">
+      CONTRATAR AGORA
+    </button>
+  );
+}
