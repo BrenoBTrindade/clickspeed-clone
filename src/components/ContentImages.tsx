197b1290247173b657bd41c232c4ab7b2d93a64e
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-13 03:03:54 -0300

    feat: content images component

--- src/components/ContentImages.tsx
+++ src/components/ContentImages.tsx
@@ -10,7 +10,7 @@ const image = [
 
 export default function ContentImages() {
   return (
-    <div className=" m-auto mt-5 flex h-72 w-[85%] items-center justify-center rounded-md bg-purple-950 p-5 shadow-md shadow-gray-700">
+    <div className=" m-auto mt-5 flex h-72 w-[85%] items-center justify-center rounded-md bg-purple-950 p-5 shadow-md shadow-gray-500">
       <div className="flex w-[80%] justify-evenly">
         {image.map((image) => (
           <Image
